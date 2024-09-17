7b7b61dd026184af6d6b1a2d7b5a8ee0db70d694
pszachta <pszachta@umich.edu>
2024-09-16 19:13:12 -0700

    Ensured plotting has perplexity for both train and validation

--- perplexity/train.py
+++ perplexity/train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Subset
 import wandb
+import matplotlib.pyplot as plt
 import pprint
 import warnings
 
@@ -77,11 +78,15 @@ if __name__ == "__main__":
     print()
 
     # Scaling Experiments
+    data_and_perplexities = []
     for fraction in [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1]:
         # Create a subset of the dataset
-        size = int(len(dataset["train"]) * fraction)
-        subset = Subset(dataset["train"], indices=range(size))
-        train_loader = DataLoader(subset, batch_size=args.batch_size, shuffle=True)
+        train_size = int(len(dataset["train"]) * fraction)
+        validation_size = int(len(dataset["validation"]) * fraction)
+        train_subset = Subset(dataset["train"], indices=range(train_size))
+        validation_subset = Subset(dataset["validation"], indices=range(validation_size))
+        train_loader = DataLoader(train_subset, batch_size=args.batch_size, shuffle=True)
+        validation_loader = DataLoader(validation_subset, batch_size=args.batch_size, shuffle=True)
 
         # model name schema
         model_name = f"{args.architecture}_dv={args.dataset_version}_df={fraction}_p={model.num_params}"
@@ -89,7 +94,7 @@ if __name__ == "__main__":
         if args.wandb_log:
             run = wandb.init(
                 project="wikitext-scaling",
-                name=f"{dataset_name}_{int(fraction*100)}%",
+                name=f"{dataset_name}_c{args.num_epochs}_d{int(fraction*100)}%",
                 group=f"{dataset_name}_transformer",
                 config={
                     "learning_rate": args.lr,
@@ -110,8 +115,22 @@ if __name__ == "__main__":
                 wandb.log({"loss": train_loss})
 
         # Evaluate Perplexity
-        perplexity = evaluate_perplexity(model, train_loader, loss_fn, DEVICE)
-        print(f"Dataset Size: {int(fraction*100)}%, Perplexity: {perplexity}\n")
+        train_perplexity = evaluate_perplexity(model, train_loader, loss_fn, DEVICE)
+        validation_perplexity = evaluate_perplexity(model, validation_loader, loss_fn, DEVICE)
+        data_and_perplexities.append((args.batch_size * len(train_loader) * args.seq_max_length, train_perplexity, validation_perplexity))
+        print(f"Dataset Size: {int(fraction*100)}%, Train Loss: {train_loss}, Train Perplexity: {train_perplexity}, Validation Perplexity: {validation_perplexity}\n")
         if args.wandb_log:
-            wandb.log({"loss": train_loss, "perplexity": perplexity})
-            wandb.finish()
+            wandb.log({"train_loss": train_loss, "train_perplexity": train_perplexity, "validation_perplexity": validation_perplexity})
+        wandb.finish()
+    data_sizes = [entry[0] for entry in data_and_perplexities]
+    train_perplexities = [entry[1] for entry in data_and_perplexities]
+    validation_perplexities = [entry[2] for entry in data_and_perplexities]
+    plt.figure(figsize=(8, 6))
+    plt.loglog(data_sizes, train_perplexities, marker="o", linestyle="-", color="blue")
+    plt.loglog(data_sizes, validation_perplexities, marker="o", linestyle="-", color="green")
+    plt.legend()
+    plt.xlabel("Data Set Size")
+    plt.ylabel("Validation Loss")
+    plt.title(model_name)
+    plt.grid(True, which="both", ls="--")
+    plt.show()
