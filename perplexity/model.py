06d9e99ca1162477bdc92bb031f861ba717f67cb
Advait <advait.gosai@gmail.com>
2024-10-18 12:14:42 -0700

    1.6m f

--- perplexity/model.py
+++ perplexity/model.py
@@ -95,10 +95,10 @@ class MetaXTransformers:
     ):
         # Predefined configurations to match target parameter counts, including varying d_ff
         self.configurations = [
-            {"d_model": 2, "n_layers": 1, "n_heads": 1, "d_ff": 8},    # ~200k params
+            # {"d_model": 2, "n_layers": 1, "n_heads": 1, "d_ff": 8},    # ~200k params
             # {"d_model": 4, "n_layers": 1, "n_heads": 1, "d_ff": 16},    # ~400k params
             # {"d_model": 8, "n_layers": 1, "n_heads": 1, "d_ff": 32},    # ~800k params
-            # {"d_model": 16, "n_layers": 1, "n_heads": 1, "d_ff": 64},    # ~1.6M params
+            {"d_model": 16, "n_layers": 1, "n_heads": 1, "d_ff": 64},    # ~1.6M params
             # {"d_model": 32, "n_layers": 1, "n_heads": 1, "d_ff": 128},    # ~3.2M params
             # {"d_model": 64, "n_layers": 2, "n_heads": 2, "d_ff": 256},    # ~6.5M params
             # {"d_model": 128, "n_layers": 4, "n_heads": 4, "d_ff": 512},   # ~14M params
