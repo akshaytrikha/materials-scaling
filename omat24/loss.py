f105f8f316ca6f488d656965f5d979411c958906
pszachta <pszachta@umich.edu>
2024-11-26 00:05:54 -0800

    Changed loss function to be L1 loss with coefficients of 20, 10, and 1

--- omat24/loss.py
+++ omat24/loss.py
@@ -1,21 +1,19 @@
 import torch.nn as nn
 
 
-def compute_mse_loss(
+def compute_mae_loss(
     pred_forces, pred_energy, pred_stress, true_forces, true_energy, true_stress, mask
 ):
-    """Compute the MSE loss for forces, energy, and stress, considering the mask."""
+    """Compute the MAE loss for forces, energy, and stress, considering the mask."""
     # Mask out padded atoms
     mask = mask.unsqueeze(-1)  # Shape: [batch_size, max_atoms, 1]
     pred_forces = pred_forces * mask.float()
     true_forces = true_forces * mask.float()
-
     # Compute MSE losses
-    force_loss = nn.MSELoss()(pred_forces, true_forces)
-    energy_loss = nn.MSELoss()(pred_energy, true_energy)
-    stress_loss = nn.MSELoss()(pred_stress, true_stress)
-
-    return force_loss + energy_loss + stress_loss
+    energy_loss = nn.L1Loss()(pred_energy, true_energy)
+    force_loss = nn.L1Loss()(pred_forces, true_forces)
+    stress_loss = nn.L1Loss()(pred_stress, true_stress)
+    return 20 * energy_loss + 10 * force_loss + stress_loss
 
 
 class CosineSimilarityLoss(nn.Module):
