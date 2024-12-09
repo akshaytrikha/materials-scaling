b3058dbb6beb7fc4106e5d7686d6cebfc2f0d9dc
pszachta <pszachta@umich.edu>
2024-12-09 13:32:09 -0800

    Adapted code for arbitrary k

--- omat24/models/naive.py
+++ omat24/models/naive.py
@@ -39,99 +39,71 @@ class NaiveAtomModel:
             )
 
             # Find nearest neighbors
-            nearest_indices = self.find_nearest_neighbors(distance_matrix)
+            tuples_of_nearest_indices = self.find_nearest_neighbors(distance_matrix)
 
             # Estimate per-atom energy contribution
             energy_per_atom = energy / num_atoms if num_atoms > 0 else 0.0
-
-            for idx, atom in enumerate(atoms):
+            for idx, _ in enumerate(atoms):
                 atom_type = symbols[idx]
-                neighbor_idx = nearest_indices[idx]
-                neighbor_type = symbols[neighbor_idx]
-
-                # Accumulate forces
-                if atom_type not in self.force_sum.keys():
-                    self.force_sum[atom_type] = {}
-                    self.force_count[atom_type] = {}
-                if neighbor_type not in self.force_sum[atom_type].keys():
-                    self.force_sum[atom_type][neighbor_type] = 0
-                    self.force_count[atom_type][neighbor_type] = 0
-                self.force_sum[atom_type][neighbor_type] += forces[idx]
-                self.force_count[atom_type][neighbor_type] += 1
-
-                # Accumulate energy
-                if atom_type not in self.energy_sum.keys():
-                    self.energy_sum[atom_type] = {}
-                    self.energy_count[atom_type] = {}
-                if neighbor_type not in self.energy_sum[atom_type].keys():
-                    self.energy_sum[atom_type][neighbor_type] = 0
-                    self.energy_count[atom_type][neighbor_type] = 0
-                self.energy_sum[atom_type][neighbor_type] += energy_per_atom
-                self.energy_count[atom_type][neighbor_type] += 1
+                tuples_of_nearest_symbols = []
+                for tuple_of_nearest_indices in tuples_of_nearest_indices[idx]:
+                    tuple_of_nearest_symbols = []
+                    for nearest_index in tuple_of_nearest_indices:
+                        neighbor_type = symbols[nearest_index]
+                        tuple_of_nearest_symbols.append(neighbor_type)
+                    tuple_of_nearest_symbols = tuple(tuple_of_nearest_symbols)
+                for tuple_of_nearest_symbols in tuples_of_nearest_symbols:
+                    # Accumulate forces
+                    if atom_type not in self.force_sum.keys():
+                        self.force_sum[atom_type] = {}
+                        self.force_count[atom_type] = {}
+                    if tuple_of_nearest_symbols not in self.force_sum[atom_type].keys():
+                        self.force_sum[atom_type][tuple_of_nearest_symbols] = 0
+                        self.force_count[atom_type][tuple_of_nearest_symbols] = 0
+                    self.force_sum[atom_type][tuple_of_nearest_symbols] += np.linalg.norm(forces[idx])
+                    self.force_count[atom_type][tuple_of_nearest_symbols] += 1
+                    # Accumulate energy
+                    if atom_type not in self.energy_sum.keys():
+                        self.energy_sum[atom_type] = {}
+                        self.energy_count[atom_type] = {}
+                    if tuple_of_nearest_symbols not in self.energy_sum[atom_type].keys():
+                        self.energy_sum[atom_type][tuple_of_nearest_symbols] = 0
+                        self.energy_count[atom_type][tuple_of_nearest_symbols] = 0
+                    self.energy_sum[atom_type][tuple_of_nearest_symbols] += energy_per_atom
+                    self.energy_count[atom_type][tuple_of_nearest_symbols] += 1
 
             # Accumulate stress
             self.stress_sum += stress
             self.stress_count += 1
 
-    def find_nearest_neighbors(self, distance_matrix):
+    def find_nearest_neighbors_tuples(self, distance_matrix, k):
         """For each atom, find the index of its nearest neighbor."""
-        nearest_indices = []
-        for i, distances in enumerate(distance_matrix):
-            # Exclude self-distance by setting it to infinity
-            distances = distances.copy()
-            distances[i] = np.inf
-            nearest = np.argmin(distances)
-            nearest_indices.append(nearest)
-        return nearest_indices
+        tuples_of_nearest_indices = []
+        for num_nearest_neighbors in range(k + 1):
+            tuple_of_nearest_indices = ()
+            for i, distances in enumerate(distance_matrix):
+                # Exclude self-distance by setting it to infinity
+                distances = distances.copy()
+                distances[i] = np.inf
+                # Get indices of the k smallest distances
+                nearest = np.argsort(distances)[:num_nearest_neighbors]
+                tuple_of_nearest_indices.append(nearest.tolist())
+            tuples_of_nearest_indices.append(tuple_of_nearest_indices)
+        return tuples_of_nearest_indices
 
     def finalize(self):
         """Compute the mean forces and energy contributions after training."""
         self.mean_forces = {}
-        overall_sum = 0
-        overall_count = 0
         for atom_type in self.force_sum.keys():
             self.mean_forces[atom_type] = {}
-            atom_type_sum = 0
-            atom_type_count = 0
-            for neighbor_type in self.force_sum[atom_type].keys():
-                if self.force_count[atom_type][neighbor_type] > 0:
-                    atom_type_sum += self.force_sum[atom_type][neighbor_type]
-                    atom_type_count += self.force_count[atom_type][neighbor_type]
-                    self.mean_forces[atom_type][neighbor_type] = self.force_sum[atom_type][neighbor_type] / self.force_count[atom_type][neighbor_type]
-            if atom_type_count == 0:
-                self.mean_forces[atom_type]["atom_type"] = 0
-            else:
-                self.mean_forces[atom_type]["atom_type"] = atom_type_sum / atom_type_count
-            overall_sum += atom_type_sum
-            overall_count += atom_type_count
-        if overall_count == 0:
-            self.mean_forces["overall"] = 0
-        else:
-            self.mean_forces["overall"] = overall_sum / overall_count
-
-        self.mean_energies = {}
-        overall_sum = 0
-        overall_count = 0
+            for tuple_of_nearest_symbols in self.force_sum[atom_type].keys():
+                if self.force_count[atom_type][tuple_of_nearest_symbols] > 0:
+                    self.mean_forces[atom_type][tuple_of_nearest_symbols] = self.force_sum[atom_type][tuple_of_nearest_symbols] / self.force_count[atom_type][tuple_of_nearest_symbols]
         for atom_type in self.energy_sum.keys():
             self.mean_energies[atom_type] = {}
-            atom_type_sum = 0
-            atom_type_count = 0
-            for neighbor_type in self.energy_sum[atom_type].keys():
-                if self.energy_count[atom_type][neighbor_type] > 0:
-                    atom_type_sum += self.energy_sum[atom_type][neighbor_type]
-                    atom_type_count += self.energy_count[atom_type][neighbor_type]
-                    self.mean_energies[atom_type][neighbor_type] = self.energy_sum[atom_type][neighbor_type] / self.energy_count[atom_type][neighbor_type]
-            if atom_type_count == 0:
-                self.mean_energies[atom_type]["atom_type"] = 0
-            else:
-                self.mean_energies[atom_type]["atom_type"] = atom_type_sum / atom_type_count
-            overall_sum += atom_type_sum
-            overall_count += atom_type_count
-        if overall_count == 0:
-            self.mean_energies["overall"] = 0
-        else:
-            self.mean_energies["overall"] = overall_sum / overall_count
-
+            for tuple_of_nearest_symbols in self.energy_sum[atom_type].keys():
+                if self.energy_count[atom_type][tuple_of_nearest_symbols] > 0:
+                    self.mean_energies[atom_type][tuple_of_nearest_symbols] = self.energy_sum[atom_type][tuple_of_nearest_symbols] / self.energy_count[atom_type][tuple_of_nearest_symbols]
         # Compute mean stress
         self.mean_stress = (
             self.stress_sum / self.stress_count
@@ -144,50 +116,47 @@ class NaiveAtomModel:
         symbols = atoms.get_chemical_symbols()
         positions = atoms.get_positions()
         num_atoms = len(atoms)
-
         if num_atoms == 0:
-            return 0.0, np.zeros((0, 3)), self.mean_stress
-
+            return 0.0, 0.0, self.mean_stress
         # Compute distance matrix
         distance_matrix = np.linalg.norm(
             positions[:, np.newaxis, :] - positions[np.newaxis, :, :], axis=-1
         )
-
         # Find nearest neighbors
-        nearest_indices = self.find_nearest_neighbors(distance_matrix)
-
+        tuples_of_nearest_indices = self.find_nearest_neighbors(distance_matrix)
         # Initialize predictions
-        predicted_forces = np.zeros((num_atoms, 3))
-        predicted_energy = 0.0
-
-        for idx, atom in enumerate(atoms):
+        predicted_forces = np.zeros((num_atoms))
+        predicted_energies = np.zeros((num_atoms))
+        for idx, _ in enumerate(atoms):
             atom_type = symbols[idx]
-            neighbor_idx = nearest_indices[idx]
-            neighbor_type = symbols[neighbor_idx]
-            # Predict forces
-            if atom_type in self.mean_forces.keys():
-                if neighbor_type in self.mean_forces[atom_type].keys():
-                    predicted_forces[idx] = self.mean_forces[atom_type][neighbor_type]
-                else:
-                    predicted_forces[idx] = self.mean_forces[atom_type]["atom_type"]
-            else:
-                # If unseen configuration, assign zero force or some default
-                predicted_forces[idx] = self.mean_forces["overall"]
-
-            # Predict energy
-            if atom_type in self.mean_energies.keys():
-                if neighbor_type in self.mean_energies[atom_type].keys():
-                    predicted_energy += self.mean_energies[atom_type][neighbor_type]
-                else:
-                    predicted_energy += self.mean_energies[atom_type]["atom_type"]
-            else:
-                # If unseen configuration, assign zero force or some default
-                predicted_energy += self.mean_energies["overall"]
+            tuples_of_nearest_symbols = []
+            for tuple_of_nearest_indices in tuples_of_nearest_indices[idx]:
+                tuple_of_nearest_symbols = []
+                for nearest_index in tuple_of_nearest_indices:
+                    neighbor_type = symbols[nearest_index]
+                    tuple_of_nearest_symbols.append(neighbor_type)
+                tuple_of_nearest_symbols = tuple(tuple_of_nearest_symbols)
+            match_found = False
+            tuple_index = len(tuples_of_nearest_symbols) - 1
+            while not match_found:
+                tuple_of_nearest_symbols = tuples_of_nearest_symbols[tuple_index]
+                # Predict forces
+                if atom_type in self.mean_forces.keys() and tuple_of_nearest_symbols in self.force_sum[atom_type].keys():
+                        predicted_forces[idx] = self.mean_forces[atom_type][tuple_of_nearest_symbols]
+                        match_found = True
+                tuple_index -= 1
+            while not match_found:
+                tuple_of_nearest_symbols = tuples_of_nearest_symbols[tuple_index]
+                # Predict energies
+                if atom_type in self.mean_forces.keys() and tuple_of_nearest_symbols in self.energy_sum[atom_type].keys():
+                        predicted_energies[idx] = self.mean_energies[atom_type][tuple_of_nearest_symbols]
+                        match_found = True
+                tuple_index -= 1
 
         # Predict stress as the mean stress from training
         predicted_stress = self.mean_stress.copy()
 
-        return predicted_energy, predicted_forces, predicted_stress
+        return predicted_energies, predicted_forces, predicted_stress
 
     def save(self, filepath):
         """Save the trained model to a file."""
