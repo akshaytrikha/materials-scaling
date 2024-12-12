763e62297910268fe77ff19fb8365eb7d48f9de6
pszachta <pszachta@umich.edu>
2024-12-12 09:53:42 -0800

    Updated test.py

--- omat24/test.py
+++ omat24/test.py
@@ -32,7 +32,7 @@ results_path = match.group("results_path")  # results path is a json file
 with open(results_path, "r") as f:
     result_json = json.load(f)
     config = result_json["298"][0]["config"]
-    losses = result_json["298"][0]["losses"]["0"]
+    losses = result_json["298"][0]["losses"]["11"]
 
 # Test config
 assert config["embedding_dim"] == 4
@@ -40,5 +40,5 @@ assert config["depth"] == 2
 assert config["num_params"] == 614
 
 # Test losses
-np.testing.assert_allclose(losses["train_loss"], 381.620849609375, rtol=0.1)
-np.testing.assert_allclose(losses["val_loss"], 425.38733927408856, rtol=0.1)
+np.testing.assert_allclose(losses["train_loss"], 327.0042724609375, rtol=0.1)
+np.testing.assert_allclose(losses["val_loss"], 355.8498229980469, rtol=0.1)
