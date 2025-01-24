7db10806874a06557c1945126927bbf275157944
Akshay Trikha <akshaytrikha@gmail.com>
2025-01-24 12:47:12 -0800

    better fcn param scaling

--- omat24/models/fcn.py
+++ omat24/models/fcn.py
@@ -6,10 +6,11 @@ class MetaFCNModels:
     def __init__(self, vocab_size=119):
         self.configurations = [
             {"embedding_dim": 4, "hidden_dim": 4, "depth": 2},
-            {"embedding_dim": 4, "hidden_dim": 4, "depth": 4},
+            # {"embedding_dim": 4, "hidden_dim": 4, "depth": 4},
             {"embedding_dim": 8, "hidden_dim": 8, "depth": 3},
-            {"embedding_dim": 16, "hidden_dim": 16, "depth": 2},
-            {"embedding_dim": 32, "hidden_dim": 32, "depth": 2},
+            {"embedding_dim": 8, "hidden_dim": 16, "depth": 3},
+            {"embedding_dim": 16, "hidden_dim": 32, "depth": 3},
+            {"embedding_dim": 32, "hidden_dim": 32, "depth": 4},
         ]
         self.vocab_size = vocab_size
 
