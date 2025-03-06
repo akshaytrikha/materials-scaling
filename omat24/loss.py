b4a68e52cecb2fd7a6bd9e45c5061a986ca0974e
Akshay Trikha <akshaytrikha@gmail.com>
2025-03-06 11:48:38 -0800

    wip

--- omat24/loss.py
+++ omat24/loss.py
@@ -107,10 +107,10 @@ def compute_loss(
 
     loss_dict = {
         "total_loss": total_loss,
-        "energy_loss": energy_loss,
-        "force_loss": force_loss,
-        "stress_iso_loss": stress_iso_loss,
-        "stress_aniso_loss": stress_aniso_loss,
+        "energy_loss": ENERGY_COEF * energy_loss,
+        "force_loss": FORCE_COEF * force_loss,
+        "stress_iso_loss": STRESS_COEF * stress_iso_loss,
+        "stress_aniso_loss": STRESS_COEF * stress_aniso_loss,
     }
 
     return loss_dict
