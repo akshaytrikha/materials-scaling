ec1936014a891d9c966c2f2d3ffb4b44ab82f763
Akshay Trikha <akshaytrikha@gmail.com>
2025-03-11 18:36:49 -0700

    update test_model_init()

--- omat24/tests/test_model_init.py
+++ omat24/tests/test_model_init.py
@@ -1,14 +1,16 @@
-import warnings
-import json
+# External
+import torch
+from pathlib import Path
+import unittest
 from collections import defaultdict
+import warnings
+import random
+import numpy as np
 
 warnings.filterwarnings(
     "ignore", message="You are using `torch.load` with `weights_only=False`"
 )
 warnings.filterwarnings("ignore", message="`torch.cuda.amp.autocast")
-# External
-import torch
-from pathlib import Path
 
 # Internal
 from data import get_dataloaders
@@ -19,26 +21,22 @@ from models.schnet import SchNet
 from models.equiformer_v2 import EquiformerS2EFS
 from train_utils import run_validation
 
-# Set seed & device
-SEED = 1024
-torch.manual_seed(SEED)
-DEVICE = torch.device("cpu")
 
 # Configuration dictionary for different model sizes
 MODEL_CONFIGS = {
     "FCN": {
         "1k": {
             "vocab_size": 119,
-            "embedding_dim": 12,
-            "hidden_dim": 12,
-            "depth": 2,
+            "embedding_dim": 16,
+            "hidden_dim": 16,
+            "depth": 1,
             "use_factorized": False,
         },
         "50k": {
             "vocab_size": 119,
-            "embedding_dim": 64,
-            "hidden_dim": 64,
-            "depth": 8,
+            "embedding_dim": 96,
+            "hidden_dim": 96,
+            "depth": 2,
             "use_factorized": False,
         },
     },
@@ -77,125 +75,164 @@ MODEL_CONFIGS = {
 }
 
 
-def run_test():
-    # args = get_args()
-    datasets = VALID_DATASETS
-    split_name = "val"
-    val_data_fraction = 0.01
-    datasets_base_path = "./datasets"
-    n_elements = 119
+class TestModelInit(unittest.TestCase):
+    def set_seed(self):
+        self.seed = 1024
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        self.device = torch.device("cpu")
 
-    # Download datasets if not present
-    dataset_paths = []
-    for dataset_name in datasets:
-        dataset_path = Path(f"{datasets_base_path}/{split_name}/{dataset_name}")
-        if not dataset_path.exists():
-            download_dataset(dataset_name, split_name, datasets_base_path)
-        dataset_paths.append(dataset_path)
+    def test_model_init(self):
+        """Test models initialize to predict means of dataset"""
+        self.set_seed()
 
-    # User Hyperparam Feedback
-    batch_size = 64
-    use_factorize = False
+        datasets = VALID_DATASETS
+        split_name = "val"
+        val_data_fraction = 0.01
+        datasets_base_path = "./datasets"
+        n_elements = 119
 
-    # Results dictionary to store validation losses
-    results = defaultdict(dict)
+        # Download datasets if not present
+        dataset_paths = []
+        for dataset_name in datasets:
+            dataset_path = Path(f"{datasets_base_path}/{split_name}/{dataset_name}")
+            if not dataset_path.exists():
+                download_dataset(dataset_name, split_name, datasets_base_path)
+            dataset_paths.append(dataset_path)
 
-    # Iterate over architectures and parameter scales
-    for architecture in ["FCN", "SchNet", "Transformer"]:
-        graph = architecture in ["SchNet", "EquiformerV2"]
+        # User Hyperparam Feedback
+        batch_size = 64
+        use_factorize = False
 
-        # Determine max_n_atoms for Transformer if needed
-        if architecture == "Transformer":
-            if split_name == "train":
-                max_n_atoms = 236
-            elif split_name == "val":
-                max_n_atoms = 168
+        # Results dictionary to store validation losses
+        results = defaultdict(dict)
 
-        for param_scale in ["1k", "50k"]:
-            config = MODEL_CONFIGS[architecture][param_scale]
+        # Iterate over architectures and parameter scales
+        for architecture in ["FCN", "SchNet", "Transformer"]:
+            graph = architecture in ["SchNet", "EquiformerV2"]
 
-            # Initialize model with appropriate config
-            if architecture == "FCN":
-                model = FCNModel(**config)
-            elif architecture == "Transformer":
-                model = XTransformerModel(**config)
-            elif architecture == "SchNet":
-                model = SchNet(**config)
+            # Determine max_n_atoms for Transformer if needed
+            if architecture == "Transformer":
+                if split_name == "train":
+                    max_n_atoms = 236
+                elif split_name == "val":
+                    max_n_atoms = 168
 
-            print(
-                f"\n{architecture} {param_scale} Model is on device {DEVICE} and has {model.num_params} parameters"
-            )
+            for param_scale in ["1k", "50k"]:
+                config = MODEL_CONFIGS[architecture][param_scale]
 
-            # Get data loaders
-            _, val_loader = get_dataloaders(
-                dataset_paths,
-                train_data_fraction=0,
-                batch_size=batch_size,
-                seed=SEED,
-                architecture=architecture,
-                batch_padded=False,
-                val_data_fraction=val_data_fraction,
-                train_workers=0,
-                val_workers=0,
-                graph=graph,
-                factorize=use_factorize,
-            )
+                # Initialize model with appropriate config
+                if architecture == "FCN":
+                    model = FCNModel(**config)
+                elif architecture == "Transformer":
+                    model = XTransformerModel(**config)
+                elif architecture == "SchNet":
+                    model = SchNet(**config)
 
-            dataset_size = len(val_loader.dataset)
-            print(f"graph: {graph}")
-            print(
-                f"\nValidating on dataset fraction {val_data_fraction} with {dataset_size} samples"
-            )
+                print(
+                    f"\n{architecture} {param_scale} Model is on device {self.device} and has {model.num_params} parameters"
+                )
 
-            # Run validation
-            (
-                val_loss,
-                val_energy_loss,
-                val_force_loss,
-                val_stress_iso_loss,
-                val_stress_aniso_loss,
-            ) = run_validation(
-                model=model,
-                val_loader=val_loader,
-                graph=graph,
-                device=DEVICE,
-                factorize=False,
-                use_mixed_precision=False,
-            )
+                # Get data loaders
+                _, val_loader = get_dataloaders(
+                    dataset_paths,
+                    train_data_fraction=0,
+                    batch_size=batch_size,
+                    seed=self.seed,
+                    architecture=architecture,
+                    batch_padded=False,
+                    val_data_fraction=val_data_fraction,
+                    train_workers=0,
+                    val_workers=0,
+                    graph=graph,
+                    factorize=use_factorize,
+                )
 
-            # Store results
-            results[architecture][param_scale] = {
-                "params": model.num_params,
-                "val_loss": val_loss,
-                "val_energy_loss": val_energy_loss,
-                "val_force_loss": val_force_loss,
-                "val_stress_iso_loss": val_stress_iso_loss,
-                "val_stress_aniso_loss": val_stress_aniso_loss,
-            }
+                dataset_size = len(val_loader.dataset)
+                print(
+                    f"\nValidating on dataset fraction {val_data_fraction} with {dataset_size} samples"
+                )
 
-            print(
-                f"\n{architecture} {param_scale} Model had initial val_loss of {val_loss}"
-            )
+                # Run validation
+                (
+                    val_loss,
+                    val_energy_loss,
+                    val_force_loss,
+                    val_stress_iso_loss,
+                    val_stress_aniso_loss,
+                ) = run_validation(
+                    model=model,
+                    val_loader=val_loader,
+                    graph=graph,
+                    device=self.device,
+                    factorize=False,
+                    use_mixed_precision=False,
+                )
 
-    # Print summary table of results
-    print("\n" + "=" * 80)
-    print("MODEL COMPARISON SUMMARY")
-    print("=" * 80)
-    print(f"{'Architecture':<15} {'Size':<10} {'Parameters':<15} {'Val Loss':<15}")
-    print("-" * 80)
+                # Store results
+                results[architecture][param_scale] = {
+                    "params": model.num_params,
+                    "val_loss": val_loss,
+                    "val_energy_loss": val_energy_loss,
+                    "val_force_loss": val_force_loss,
+                    "val_stress_iso_loss": val_stress_iso_loss,
+                    "val_stress_aniso_loss": val_stress_aniso_loss,
+                }
 
-    for arch in results:
-        for size in results[arch]:
-            print(
-                f"{arch:<15} {size:<10} {results[arch][size]['params']:<15} {results[arch][size]['val_loss']:<15.6f}"
-            )
+        # Print summary table of results
+        print("\n" + "=" * 80)
+        print("MODEL COMPARISON SUMMARY")
+        print("=" * 80)
+        print(f"{'Architecture':<15} {'Size':<10} {'Epoch 0 Val Loss':<15}")
+        print("-" * 80)
 
-    # # Save results to file
-    # with open("model_comparison_results.json", "w") as f:
-    #     json.dump(results, f, indent=2)
+        for arch in results:
+            for size in results[arch]:
+                print(f"{arch:<15} {size:<10} {results[arch][size]['val_loss']:<15.6f}")
 
-    # print("\nResults saved to model_comparison_results.json")
+        # Check FCN
+        self.assertAlmostEqual(
+            results["FCN"]["1k"]["val_loss"],
+            61.95274842896077,
+            places=3,
+            msg="FCN 1k epoch 0 val_loss is incorrect",
+        )
+        self.assertAlmostEqual(
+            results["FCN"]["50k"]["val_loss"],
+            65.03901063433345,
+            places=3,
+            msg="FCN 50k epoch 0 val_loss is incorrect",
+        )
 
+        # Check Transformer
+        self.assertAlmostEqual(
+            results["Transformer"]["1k"]["val_loss"],
+            53.34798036895183,
+            places=3,
+            msg="Transformer 1k epoch 0 val_loss is incorrect",
+        )
+        self.assertAlmostEqual(
+            results["Transformer"]["50k"]["val_loss"],
+            59.72025161648389,
+            places=3,
+            msg="Transformer 50k epoch 0 val_loss is incorrect",
+        )
 
-if __name__ == "__main__":
-    run_test()
+        # Check SchNet
+        self.assertAlmostEqual(
+            results["SchNet"]["1k"]["val_loss"],
+            51.9914984021868,
+            places=3,
+            msg="SchNet 1k epoch 0 val_loss is incorrect",
+        )
+        self.assertAlmostEqual(
+            results["SchNet"]["50k"]["val_loss"],
+            52.018605084152696,
+            places=3,
+            msg="SchNet 50k epoch 0 val_loss is incorrect",
+        )
